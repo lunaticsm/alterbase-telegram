57b6c040cf27d409d17138c259cabdd1d1cca194
luna <novitaanggraini_5235151423@mhs.unj.ac.id>
2022-12-08 19:00:01 +0700

    test

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
+from aiohttp import web
+from plugins import web_server
+
 import config, sys, os, requests
 
 from plugins import Database
 from pyrogram import Client, enums
-from pyrogram.types import BotCommand, BotCommandScopeAllPrivateChats
+from pyrogram.types import BotCommand, BotCommandScopeAllPrivateChats, PORT
 
 data = []
 
@@ -67,6 +70,12 @@ class Bot(Client):
 
         print('BOT TELAH AKTIF')
     
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
     async def stop(self):
         await super().stop()
         print('BOT BERHASIL DIHENTIKAN')
--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,2 +1,11 @@
 from .helper import Helper, Waktu
 from .database import Database
+#@iryme
+from aiohttp import web
+from .route import routes
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
